ce7bcc70bded372dc0a2a9d564b363d87e6df745
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2021-03-02 12:11:20 +0100

    Update courses.js

--- routers/courses.js
+++ routers/courses.js
@@ -5,4 +5,11 @@ router.get('', async (req,res)=>{
     res.send(await Course.find());
 });
 
+router.get('/:id', async (req,res)=>{
+    let course = await Course.findById(req.params.id);
+    if(!course)
+        return res.status(404).send('Id not found')
+    res.send();
+});
+
 module.exports = router
